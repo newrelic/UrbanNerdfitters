1dac8f29ebe997f16182db289d2b6a60d262cfd1
Jemiah Sius <jsius@newrelic.com>
2019-07-17 10:45:38 -0700

    endpoint updates for EC2

--- frontend/config.js
+++ frontend/config.js
@@ -1,3 +1,4 @@
 // This is client side config only - don't put anything in here that shouldn't be public!
-export const endpoint = `http://localhost:4444`;
+// export const endpoint = `http://localhost:4444`;
+export const endpoint = `http://ec2-18-217-126-238.us-east-2.compute.amazonaws.com:4444`;
 export const perPage = 6;
